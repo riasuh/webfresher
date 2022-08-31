a551337a52e80cd79e7e2d170fca39a8713da308
riasuh <milo099sas@gmail.com>
2022-08-31 22:14:55 +0200

    bru

--- docs/.vuepress/.temp/pages/home.html.vue
+++ docs/.vuepress/.temp/pages/home.html.vue
@@ -1,6 +1,9 @@
 <template><div><h1 id="home" tabindex="-1"><a class="header-anchor" href="#home" aria-hidden="true">#</a> Home</h1>
 <p>Welcome Gamers!
-<img src="~@images/flag.png" alt="Logged In"></p>
+<img src="~@images/flag.png" alt="Logged In">
+eee
+eee
+eeee</p>
 </div></template>
 
 
