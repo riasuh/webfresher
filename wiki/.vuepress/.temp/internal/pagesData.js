54637d6dbe0dc058a4626b9d755a4e28ff8f0e94
riasuh <milo099sas@gmail.com>
2022-08-31 19:10:16 +0200

    commit

---
+++ wiki/.vuepress/.temp/internal/pagesData.js
@@ -0,0 +1,4 @@
+export const pagesData = {
+  // path: /404.html
+  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"c:/private/gitprojects/webproject/webproject/wiki/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
+}
